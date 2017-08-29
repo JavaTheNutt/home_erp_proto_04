ea4ff04f0fab8ebab248ad61edf9352564c786c0
Joe Wemyss <support@javathenutt.com>
2017-08-29 12:55:36 +0100

    about to insert mockery

---
+++ test/unit/services/userGroupService_test.js
@@ -0,0 +1,14 @@
+const chai = require('chai');
+const expect = chai.expect;
+const sinon = require('sinon');
+chai.use(require('sinon-chai'));
+
+const userGroupService = require('../../../api/services/userGroupService');
+
+describe('user group service', function () {
+	describe('data formatting', function () {
+		it('should successfully create a userGroup', function () {
+
+		})
+	})
+})
