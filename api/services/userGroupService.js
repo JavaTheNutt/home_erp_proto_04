01a8560fdb29741cf1609df7e9e7528819978b69
Joe Wemyss <support@javathenutt.com>
2017-08-30 01:24:29 +0100

    tested usergroup service

---
+++ api/services/userGroupService.js
@@ -0,0 +1,42 @@
+const UserGroup      = require('../models/db/UserGroup');
+const Logger         = require('../util/Logger')('USER_GROUP_SERVICE');
+const emailValidator = require('../services/validation').validateEmail;
+module.exports       = {
+	async createGroup(details) {
+		'use strict';
+		Logger.info(`create group called`);
+		const group   = new UserGroup(this.formatGroup(details));
+		const result = await group.save();
+		Logger.verbose(`results of save: ${JSON.stringify(result)}`);
+		return result;
+	},
+	formatGroup(details) {
+		'use strict';
+		Logger.info(`attempting to format a group`);
+		Logger.verbose(`group: ${JSON.stringify(details)}`);
+		if (!details.name) {
+			Logger.warn(`no name found to format group, throwing error`);
+			throw new Error('no valid group name provided');
+		}
+		if (!details.users || !Array.isArray(details.users) || details.users.length < 1) {
+			Logger.warn(`no users found to format group, throwing error`);
+			throw new Error('at least one user must be provided to create the group');
+		}
+		const returnableDetails = {
+			name: details.name,
+			users: []
+		};
+		details.users.forEach(user => {
+			if (!user.email || !emailValidator(user.email)) {
+				Logger.warn(`no user email found to format group, throwing error`);
+				throw new Error('no valid user email provided');
+			}
+			returnableDetails.users.push({
+				email: user.email,
+				firstName: user.firstName,
+				surname: user.surname
+			});
+		});
+		return returnableDetails;
+	}
+};
